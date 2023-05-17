0b8a77ba422a8d12d43120d7f6e9dbc9382767cd
EiadSS <115057163+EiadSS@users.noreply.github.com>
2023-05-17 13:07:00 -0400

    Add files via upload

---
+++ BoggleTests.java
@@ -0,0 +1,94 @@
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+
+import boggle.*;
+import boggle.Dictionary;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BoggleTests {
+
+    //BoggleGame  Test
+    @Test
+    void findAllWords_small() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        BoggleGame game = new BoggleGame();
+        Method method = game.getClass().getDeclaredMethod("findAllWords", Map.class, Dictionary.class, BoggleGrid.class);
+        method.setAccessible(true);
+
+        Dictionary boggleDict = new Dictionary("wordlist.txt");
+        Map<String, ArrayList<Position>> allWords = new HashMap<>();
+        BoggleGrid grid = new BoggleGrid(4);
+        grid.initalizeBoard("JFLMBRLMBCYHJGFD");
+        Object r = method.invoke(game, allWords, boggleDict, grid);
+
+        Set<String> expected = new HashSet<>(Arrays.asList("GHOST", "HOST", "THIN"));
+        assertEquals(expected, allWords.keySet());
+    }
+
+    //Dictionary Test
+    @Test
+    void containsWord() {
+        Dictionary dict = new Dictionary("wordlist.txt");
+        assertTrue(dict.containsWord("ENZYME"));
+        assertTrue(dict.isPrefix("pench"));
+    }
+
+    @Test
+    void isPrefix() {
+        Dictionary dict = new Dictionary("test.txt");
+        assertTrue(dict.containsWord("bill"));
+        assertTrue(dict.containsWord("Bill"));
+        assertTrue(dict.isPrefix("b"));
+        assertTrue(dict.isPrefix("bill"));
+        assertTrue(dict.isPrefix("bIll"));
+        assertTrue(dict.containsWord("biLL"));
+        assertFalse(dict.containsWord("idk"));
+        assertFalse(dict.isPrefix("idk"));
+    }
+    @Test
+    void posEq(){
+        BoggleGame eid = new BoggleGame();
+        ArrayList<Position> positions = new ArrayList<>();
+        positions.add(new Position(1, 2));
+        assertFalse(eid.posEqual(positions, new Position(1, 2)));
+    }
+
+    //BoggleGrid Test
+    @Test
+    void setupBoard() {
+        BoggleGrid grid = new BoggleGrid(10);
+        String letters = "";
+        for (int i = 0; i < 10; i++) {
+            letters = letters + "0123456789";
+        }
+
+        grid.initalizeBoard(letters);
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                assertEquals(letters.charAt(i*10+j), grid.getCharAt(i, j));
+            }
+        }
+    }
+
+    //BoggleStats Test
+    @Test
+    void endRoundTest() {
+        BoggleStats stats = new BoggleStats();
+        stats.endRound();
+        stats.endRound();
+        stats.endRound();
+        assertEquals(3, stats.getRound());
+    }
+
+}
